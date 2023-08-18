243326c9c0ea210e2b1d285351bd08b304dc6f1b
Amine Boucenna <89776217+amineboucenna@users.noreply.github.com>
2023-08-18 19:22:31 +0100

    Create spellcheck.py

---
+++ Course 2 Programming in Python/Week 4/spellcheck.py
@@ -0,0 +1,18 @@
+# DON'T CHANGE THIS FILE
+def word_count(sentence):
+    words = len(sentence.split())
+    print(words)
+    return words
+
+def char_count(sentence):
+    chars = len(sentence)
+    print(chars)
+    return chars
+
+def first_char(sentence):
+    first = sentence[0]
+    return first
+
+def last_char(sentence):
+    last = sentence[-1]
+    return last
